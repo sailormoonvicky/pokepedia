8a4d8bea7a3b6448e6e6d2f14d42fdaaa24c87d5
sailormoonvicky <victoriahuhh@gmail.com>
2024-03-13 16:10:19 +0000

    complete the function and add styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 import './App.css'
 
@@ -18,41 +18,69 @@ interface Pokemon{
 };
 
 function App() {
-const [pokemonName, setPokemonName] = useState<string>("");
+// const [pokemonName, setPokemonName] = useState<string>("");
 const [pokemon, setPokemon] = useState<Pokemon>();
+const [loading, setLoading] = useState<boolean>(false);
+const [error, setError] =useState<boolean>(false);
 
-useEffect(() =>{
-  if(pokemonName) {
+
+const handleSubmit:React.FormEventHandler<HTMLFormElement> = event =>{
+  event.preventDefault();
+  setPokemon(undefined);
+  setError(false);
+  const formData = new FormData(event.target as HTMLFormElement);
+  const pokemonName = formData.get("PokemonName")
+  if(pokemonName){
+    setLoading(true);
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .then((response) => response.json())
-      .then(data =>{
-        setPokemon(data)
-      })
+    .then((response) => response.json())
+    .then((data) => setPokemon(data))
+    .catch(() => setError(true))
+    .finally(() => setLoading(false))
   }
-}, [pokemonName])
+  
+}
+
 
   return (
     <main>
-      <form >
-        <label >
-          Enter pokemon name:
-          <br />
-          <input type="text" value={pokemonName} onChange={(event) => setPokemonName(event.target.value)} />
-        </label>
-      </form>
-      {pokemon &&
-      <div>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-        <h1>{pokemon.name}</h1>
-        <h2>Types: {pokemon.types.map(data => data.type.name).join(" and ")}</h2>
-        <p>Height: {pokemon.height} Weight: {pokemon.weight}</p>
-        <p>Base experience: {pokemon.base_experience}</p>
+      <div className='heading-container'>
+        <h1>Welcome to Pokepedia!</h1>
+        <p>Using the latest React technologies, Pokepedia seamlessly integrates with the Pokémon API to fetch and display an array of data on your favorite Pokémon.</p>
+        <p>Start your journey today with a simply click and see what fascinating discoveries await you in Pokepedia!</p>
       </div>
-      }
-      {!pokemon &&
-        <p>Loading...</p>}
+
+      <div className='container'>
+        <form onSubmit={handleSubmit}>
+          <label >
+            Enter Pokemon name:
+            <br/>
+            <input type="text" name='PokemonName' />
+          </label>
+          <button type='submit' >Get info!</button>
+        </form>
+        {pokemon &&
+        <div className='data-container'>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+          <h2>{pokemon.name}</h2>
+          <h3>Types: {pokemon.types.map(data => data.type.name).join(" and ")}</h3>
+          <p>Height: {pokemon.height} Weight: {pokemon.weight}</p>
+          <p>Base experience: {pokemon.base_experience}</p>
+        </div>
+        }
+        <div className='loading'>
+        {loading &&
+          <img src='./src/assets/loading.png' alt='Loading Image'></img>}
+        </div>
+        
+        <div className='error'>
+        {error &&
+          <p>Couldn't find your pokemon!</p>}
+        </div>
+
+      </div>
     </main>
-   
+
   );
 }
 
